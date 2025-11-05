2043970680100e5470715369ee0ccfc81028779e
lastborn6688 <Ugochukwudanielmatthew@gmail.com>
2025-11-05 14:07:27 +0100

    updated the beautifier to work better

--- models/Product.js
+++ models/Product.js
@@ -14,7 +14,7 @@ const productSchema = new mongoose.Schema(
         "carcharger",
         "bags",
         "bavin",
-        "baeutifiers",
+        "beautifiers",
         "cables",
         "chargers",
         "earbuds",
